a1371fd066572f92a337131ef813413f8e730578
Rachel Krohn <rkrohn@nd.edu>
2018-09-17 13:23:06 -0400

    starting code to load reddit and exogenous data for one of the three use cases

---
+++ load_model_data.py
@@ -0,0 +1,15 @@
+
+import file_utils
+
+#load all reddit and exogenous data, convert to fun python structures, and save as pickles
+#code = {cyber, crypto, cve}, indicating reddit data to load
+def load_model_data(code):
+
+	if code == "cyber":
+		sldkf
+
+	elif code == "crypto":
+		print("no data for you")
+
+	else:		#cve
+		print("no data for you")
