635be33db6d0a2ad5794320d0b5e2d969f490692
Rachel Krohn <rkrohn@nd.edu>
2018-09-18 12:26:37 -0400

    reading all cyber data

--- load_model_data.py
+++ load_model_data.py
@@ -3,24 +3,47 @@ import file_utils
 import tarfile
 import os
 
-#load all reddit and exogenous data, convert to fun python structures, and save as pickles
+#load all reddit data (comments and posts), and save as pickles
 #code = {cyber, crypto, cve}, indicating reddit data to load
-def load_model_data(code):
+def load_reddit_data(code):
 
 	if code == "cyber":
+		#load comments, either from cached pickle or directly from data
+		if os.path.exists("data_cache/%s_comments.pkl" % code):
+			#load from pickle
+			print("Loading comments from data_cache")
+			comments = file_utils.load_pickle("data_cache/%s_comments.pkl" % code)
+			print("   Loaded", len(comments))
+		#load directly, and save pickle for later
+		else:
+			print("Loading comments from source")
+			#extract comment files from tar if not already
+			if os.path.isdir("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent") == False:
+				tar = tarfile.open("../2018DecCP/Reddit/Cyber/Tng_an_RC_Cyber_sent.tar")
+				tar.extractall("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent")
+				tar.close()
+			#load each comment file
+			comments = []
+			for filename in sorted(os.listdir("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent")):
+				#get list of comments from this file
+				data = file_utils.load_zipped_multi_json("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent/" + filename)
+				#add them all to a single list
+				comments.extend(data)
+			#dump comments
+			save_comments(code, comments)
 
-		#extract comment files from tar if not already
-		if os.path.isdir("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent") == False:
-			tar = tarfile.open("../2018DecCP/Reddit/Cyber/Tng_an_RC_Cyber_sent.tar")
-			tar.extractall("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent")
-			tar.close()
-		#load each comment file
-		for filename in sorted(os.listdir("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent")):
-			data = file_utils.load_zipped_multi_json("../2018DecCP/Reddit/Cyber/UNPACK_Tng_an_RC_Cyber_sent/" + filename)
-			print(len(data))
-			print(data[0])
-			break
-
+		#load posts/submissions, either from cached pickle or directly from data
+		if os.path.exists("data_cache/%s_posts.pkl" % code):
+			#load from pickle
+			print("Loading posts from data_cache")
+			posts = file_utils.load_pickle("data_cache/%s_posts.pkl" % code)
+			print("   Loaded", len(posts))
+		#load directly, and save pickle for later
+		else:
+			print("Loading posts from source")
+			#load json.gz file
+			posts = file_utils.load_zipped_json("../2018DecCP/Reddit/Cyber/Tng_an_RS_Cyber_sent.json.gz")
+			#save to pickle
 
 	elif code == "crypto":
 		print("no data for you")
@@ -28,9 +51,32 @@ def load_model_data(code):
 	else:		#cve
 		print("no data for you")
 
+#end load_reddit_data
+
+#load all exogenous data (content depends on use case), and save as pickles
+#code = {cyber, crypto, cve}, indicating exogenous data to load
+def load_exogenous_data(code):
+
+#end load_exogenous_data
+
+#save all loaded comments to pickle
+def save_comments(code, comments):
+	#save all comments to pickle
+	print ("Loaded", len(comments), "comments, saving to data_cache/%s_comments.pkl" % code)
+	if not os.path.exists("data_cache"):
+		os.makedirs("data_cache")
+	file_utils.save_pickle(comments, "data_cache/%s_comments.pkl" % code)
+	print("   Comments saved")
+#end save_comments
+
+#save all loaded posts to pickle
+def save_posts(code, posts):
+	print("Loaded", len(posts), "posts, saving to data_cache/%s_posts.pkl" % code)
+	if not os.path.exists("data_cache"):
+		os.makedirs("data_cache")
+	file_utils.save_pickle(posts, "data_cache/%s_posts.pkl" % code)
+	print("   Posts saved")
+#end save_posts
 
 
-
-
-
-load_model_data("cyber")
+load_reddit_data("cyber")
