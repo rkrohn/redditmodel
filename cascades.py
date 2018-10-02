4b740c218666f4d4b0caa278c01ea75b09b8c6fc
Rachel Krohn <rkrohn@nd.edu>
2018-10-02 12:00:30 -0400

    finally working, including save of results

--- cascades.py
+++ cascades.py
@@ -1,11 +1,24 @@
 import data_utils
 import file_utils
+import os
 
 #given a list of posts and a list of comments, reconstruct the post/comment (cascade) structure
 #store cascade in the following way using a dictionary
 #	post id -> post object
 # 	post/comment replies field -> list of direct replies
 def build_cascades(posts, comments, code):
+	#if cascades already exist, read from cache
+	if os.path.exists("data_cache/%s_cascades/%s_cascade_posts.pkl" % (code, code)) and os.path.exists("data_cache/%s_cascades/%s_cascade_comments.pkl" % (code, code)):
+		#load from pickle
+		print("Loading cascades from data_cache")
+		return cascades, comments, missing_posts, missing_comments
+		cascades = file_utils.load_pickle("data_cache/%s_cascades/%s_cascade_posts.pkl" % (code, code))
+		comments = file_utils.load_pickle("data_cache/%s_cascades/%s_cascade_comments.pkl" % (code, code))
+		missing_posts = file_utils.load_json("data_cache/%s_cascades/%s_missing_posts.json" % (code, code))
+		missing_comments = file_utils.load_json("data_cache/%s_cascades/%s_missing_comments.json" % (code, code))
+		print("   Loaded", len(cascades))
+		return cascades, comments, missing_posts, missing_comments
+
 	print("Extracting post/comment structure for", len(posts), "and", len(comments), "comments")
 
 	#add replies field to all posts/comments, init to empty list
@@ -37,8 +50,6 @@ def build_cascades(posts, comments, code):
 	#and do the same with the comments
 	comments = data_utils.list_to_dict(comments, "id_h")
 
-	print("dicts contain", len(posts), "posts and", len(comments), "comments")
-
 	#now that we can find posts and comments at will, let's build the dictionary!
 	
 	#loop all comments, assign to immediate parent and increment comment_count of post parent
@@ -47,59 +58,44 @@ def build_cascades(posts, comments, code):
 	missing_posts = set()		#missing posts
 	for comment_id, comment in comments.copy().items():
 		#get immediate parent (post or comment)
-		#print("comment", comment_id)
 		direct_parent = comment['parent_id_h'][3:]
 		direct_parent_type = "post" if comment['parent_id_h'][:2] == "t3" else "comment"
-		#print("   direct parent", direct_parent, direct_parent_type)
 		#get post parent
 		post_parent = comment['link_id_h'][3:]
-		#print("   post parent", post_parent)
-		print(comment_id, "->", direct_parent, direct_parent_type, "->", post_parent)
 		comment_count += 1
 
 		#add this comment to replies list of immediate parent
 		try:
 			#if post parent missing, create placeholder
 			if post_parent not in cascades:
-				print("post parent fail")
 				cascades[post_parent] = create_object(post_parent, post_fields)
 				missing_posts.add(post_parent)
 			#update overall post comment count
 			cascades[post_parent]['comment_count'] += 1
-			print("post parent good")
 
 			#now handle direct parent, post or comment
 			#parent is post
 			if direct_parent_type == "post":
 				#missing post, create placeholder to hold replies
 				if direct_parent not in cascades:
-					print("direct parent (post) fail")
 					cascades[direct_parent] = create_object(direct_parent, post_fields)
 					missing_posts.add(direct_parent)
 				#add this comment to replies field of post
 				cascades[direct_parent]['replies'].append(comment_id)
-				print("direct parent (post) good")
 			#parent is comment
 			else:	
 				#missing comment, create placeholder to contain replies, point to parent post by default
 				if direct_parent not in comments:
-					print("direct parent (comment) fail")
 					temp = create_object(direct_parent, comment_fields)
-					print(temp)
 					comments[direct_parent] = temp
-					print("here1")
 					#point this placeholder comment to the top-level post
 					comments[direct_parent]['link_id_h'] = post_parent
 					cascades[post_parent]['comment_count'] += 1		#add manufactured comment to counter
 					cascades[post_parent]['replies'].append(direct_parent)	#and add to replies
-					print("here2")
 					comments[direct_parent]['parent_id_h'] = post_parent
-					print("here3")
 					missing_comments.add(direct_parent)
-					print("here4")
 				#add current comment to replies field of parent comment
 				comments[direct_parent]['replies'].append(comment_id)
-				print("direct parent (comment) good")
 		except:
 			print("FAIL")
 			print(len(missing_posts), "posts")
@@ -108,10 +104,10 @@ def build_cascades(posts, comments, code):
 				if field != "replies":
 					print(field, comment[field])
 			exit(0)
-	print("processed", comment_count,  "comments\n")
 
-	print(len(missing_posts), "missing posts")
-	print(len(missing_comments), "missing comments")
+	print("\nProcessed", comment_count,  "comments")
+	print("   ", len(missing_posts), "missing posts")
+	print("   ", len(missing_comments), "missing comments")
 
 	#verify the above process, a couple different ways
 
@@ -130,11 +126,14 @@ def build_cascades(posts, comments, code):
 	'''
 
 	#save cascades for later loading
-	print("Saving cascades to data_cache/%s_cascade_<file contents>.pkl" % code)
-	file_utils.save_pickle(cascades, "data_cache/%s_cascade_posts.pkl" % code)
-	file_utils.save_pickle(comments, "data_cache/%s_cascade_comments.pkl" % code)
-	file_utils.save_pickle(missing_posts, "data_cache/%s_cascade_missing_posts.pkl" % code)
-	file_utils.save_pickle(missing_comments, "data_cache/%s_cascade_missing_comments.pkl" % code)
+	#make sure directory exists
+	if not os.path.exists("data_cache/%s_cascades" % code):
+		os.makedirs("data_cache/%s_cascades" % code)
+	print("Saving cascades to data_cache/%s_cascades/%s_cascade_<file contents>.pkl" % (code, code))
+	file_utils.save_pickle(cascades, "data_cache/%s_cascades/%s_cascade_posts.pkl" % (code, code))
+	save_cascade_comments(code, comments)		#cascade comments
+	file_utils.save_json(list(missing_posts), "data_cache/%s_cascades/%s_cascade_missing_posts.json" % (code, code))
+	file_utils.save_json(list(missing_comments), "data_cache/%s_cascades/%s_cascade_missing_comments.json" % (code, code))
 
 	return cascades, comments, missing_posts, missing_comments
 #end build_cascades
@@ -164,7 +163,27 @@ def create_object(identifier, fields):
 	obj['replies'] = []
 	if 'comment_count' in fields:
 		obj['comment_count'] = 0
-	print("created")
-	print(obj)
 	return obj
 #end create_object
+
+#save cascade comments to pickle
+def save_cascade_comments(code, comments):
+	#save all comments to pickle
+	if not os.path.exists("data_cache/%s_cascades" % code):
+		os.makedirs("data_cache/%s_cascades" % code)
+	#break cyber comments into separate files, because memory error
+	if code == "cyber":
+		temp = {}		#temporary dictionary to hold a chunk of comments
+		count = 0
+		for comment_id, comment in comments.items():
+			temp[comment_id] = comment
+			count += 1
+			if count % 1000000 == 0:
+				file_utils.save_pickle(temp, "data_cache/%s_cascades/%s_cascade_comments_%s.pkl" % (code, code, count//1000000))
+				temp = {}
+		#last save
+		file_utils.save_pickle(temp, "data_cache/%s_cascades/%s_cascade_comments_%s.pkl" % (code, code, count//1000000))
+	else:
+		file_utils.save_pickle(comments, "data_cache/%s_cascades/%s_cascade_comments.pkl" % code)
+	print("   Comments saved")
+#end save_comments
