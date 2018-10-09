45012a88342269bdfa80d0c46beb1a38b0369f1f
Rachel Krohn <rkrohn@nd.edu>
2018-10-09 18:33:34 -0400

    use Node and CascadeTree to plot the tree (still just two comment levels for now)

--- hawkes_tree.py
+++ hawkes_tree.py
@@ -1,6 +1,8 @@
 from MHP import MHP
 import numpy as np
 import os
+from CascadeTree import CascadeTree
+from Node import Node
 
 #create directory for output plots if it does not exist
 if not os.path.exists("tree_plots"):
@@ -31,20 +33,37 @@ P.plot_rates(filename="tree_plots/multivariate_events_and_rates.png")
 
 
 #build a tree?
+SIM_TIME = 60		#time units to simulate
+
+root = Node('POST', None)	#root has no parent
 
 #first, get top-level comments - ie, replies to the post
 #just some random params for now that (hopefully) die off eventually
 P = MHP(mu = [0.05], alpha = [[0.5]], omega = 0.75)
-P.generate_seq(60, init_event = True)
+P.generate_seq(SIM_TIME, init_event = True)
 #print(P.data[:,0])
 P.plot_rates(filename="tree_plots/root_test.png", horizon = 60)
 #not really dying, but let's go with that for now
 
+#add these comments as children of the root
+root.add_children(P.data[:,0])
+
 #now, can we generate child events for each of those parent comments? following a different distribution?
-for parent in P.data[:, 0]:
-	print(parent)
-	subP = MHP(mu = [0.1], alpha = [[0.5]], omega = 1)
-	subP.generate_seq(60)	#no initial event here (can't comment at the same time as the parent goes up!)
+for parent in root.children:
+	print(parent.id)
+	subP = MHP(mu = [0.1], alpha = [[0.75]], omega = 1)
+	#no initial event here (can't comment at the same time as the parent goes up!)
+	#only simulate for time remaining in our sim period
+	subP.generate_seq(SIM_TIME - parent.id)
+	#offset times by parent time
+	for i in range(subP.data.shape[0]):
+		subP.data[i][0] += parent.id
 	print("   ", subP.data[:,0])
 
+	#add child nodes
+	parent.add_children(subP.data[:,0])
 
+
+#plot the tree!
+tree = CascadeTree(root)
+tree.viz_graph("tree.png")
