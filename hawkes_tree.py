9f974bb516e240c1088baa1ae74826e1d494f162
Rachel Krohn <rkrohn@nd.edu>
2018-10-09 18:54:23 -0400

    recursively generating comments, tree dies off by dividing mu by the level as we recurse

--- hawkes_tree.py
+++ hawkes_tree.py
@@ -39,30 +39,48 @@ root = Node('POST', None)	#root has no parent
 
 #first, get top-level comments - ie, replies to the post
 #just some random params for now that (hopefully) die off eventually
-P = MHP(mu = [0.05], alpha = [[0.5]], omega = 0.75)
+P = MHP(mu = [0.5], alpha = [[0.5]], omega = 0.75)
 P.generate_seq(SIM_TIME, init_event = True)
-#print(P.data[:,0])
-P.plot_rates(filename="tree_plots/root_test.png", horizon = 60)
 #not really dying, but let's go with that for now
 
 #add these comments as children of the root
 root.add_children(P.data[:,0])
 
+nodes = [[x, 1] for x in root.children]		#list of nodes to process (ie, generate children for)
+
 #now, can we generate child events for each of those parent comments? following a different distribution?
-for parent in root.children:
-	print(parent.id)
-	subP = MHP(mu = [0.1], alpha = [[0.75]], omega = 1)
+curr_level = 0
+nodes_at_level = 1
+for node_pair in nodes:
+	node = node_pair[0]
+	level = node_pair[1]
+
+	if level != curr_level:
+		print("level", curr_level, ":", nodes_at_level, "nodes")
+		curr_level = level
+	else:
+		nodes_at_level += 1
+
+	if level == 15:
+		print("too deep, terminating")
+		break
+
+	#init Hawkes process - taper the mu parameter off so the comment tree dies off as we get deeper
+	subP = MHP(mu = [0.05 / level], alpha = [[0.75]], omega = 1)
+
+	#generate event sequence
 	#no initial event here (can't comment at the same time as the parent goes up!)
 	#only simulate for time remaining in our sim period
-	subP.generate_seq(SIM_TIME - parent.id)
-	#offset times by parent time
+	subP.generate_seq(SIM_TIME - node.id)
+	#offset times by parent time, so they occur after
 	for i in range(subP.data.shape[0]):
-		subP.data[i][0] += parent.id
-	print("   ", subP.data[:,0])
+		subP.data[i][0] += node.id
 
-	#add child nodes
-	parent.add_children(subP.data[:,0])
+	#add these new child nodes
+	node.add_children(subP.data[:,0])
 
+	#and add the new node to list of nodes to process
+	nodes.extend([[x, level+1] for x in node.children])
 
 #plot the tree!
 tree = CascadeTree(root)
