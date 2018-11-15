dc19c0d9d94c9ac31b2cac8fbbcecc480030409a
Rachel Krohn <rkrohn@nd.edu>
2018-11-15 15:27:03 -0500

    for now, pull params from fitted - since dry run seeds are represented in the data set; given params, simulate a cascade for each seed, create desired output format, and dump everything to output file

--- predict_cascades.py
+++ predict_cascades.py
@@ -4,11 +4,15 @@
 import file_utils
 from newParamGraph import ParamGraph
 import cascade_manip
+import sim_tree
 
 import sys
 import json
 from collections import defaultdict
+import random
 
+
+DISPLAY = False		#toggle debug print statements
 
 #creates submission json file
 #domain - simulation domain, must be one of 'crypto', 'cyber', or 'CVE'
@@ -40,6 +44,8 @@ from collections import defaultdict
 	 }
 """
 def reddit_sim_to_json(domain, events, outfile):
+    print("Saving results to", outfile + "...")
+
 	#header values
     team_name = 'usc'
     scenario = 2
@@ -60,6 +66,8 @@ def reddit_sim_to_json(domain, events, outfile):
     with open(outfile,'w') as f:
         json.dump(submission, f)
 
+    print("Done")
+
     return
 #end reddit_sim_to_json
 
@@ -79,6 +87,77 @@ def load_reddit_seeds(filename):
 
     return data
 #end load_reddit_seeds
+
+
+#given a sim_tree return (tree structure of comment times), and original seed post, convert to desired dictionary structure
+#{"rootID": "t3_-dt8ErhaKuULHekBf_ke3A", "communityID": "t5_2s3qj", "actionType": "post", "parentID": "t3_-dt8ErhaKuULHekBf_ke3A", "nodeTime": "1421194091", "nodeUserID": "XHa80wD0LQJNlMcrUD32pQ", "nodeID": "t3_-dt8ErhaKuULHekBf_ke3A"}
+'''
+	"rootID": id of root post for cascade
+	"actionType": 'post' or 'comment'
+	"parentID": id of parent (comment or post), null/self if this object is post 
+	"nodeTime": timestamp of event
+	"nodeUserID": id of user posting this event
+	"nodeID": id of post/comment
+	"communityID": id of subreddit			???
+'''
+def build_cascade_events(root, post, user_ids):
+	#get start of event structure by converting sim tree to events list
+	events = tree_to_events(root, post['id_h'], post['created_utc']) 
+
+	#set the rest of the fields, the same for all events in this cascade
+	for event in events:
+		event['rootID'] = post['id_h']
+		event['communityID'] = post['subreddit_id']
+		#assign user to each event - random from known users for now
+		event['nodeUserID'] = post['author_h'] if event['nodeID'] == post['id_h'] else random.choice(user_ids)
+
+	if DISPLAY:
+		print(post['id_h'], post['author_h'], post['created_utc'])
+		print("")
+		for event in events:
+			print(event)
+		print("")
+
+	return events
+#end build_cascade_events
+
+
+NEXT_ID = 0		#global counter of next id to assign to comments
+
+#given root of sim tree, return as list of dictionary events, with correct parent pointers
+def tree_to_events(root, seedID, seedTime):
+	global NEXT_ID
+
+	visited = set()    #set of visited nodes
+	stack = [(root, None)]     #node processing stack
+
+	events = []		#list of events, where each event is a dictionary
+
+	while len(stack) != 0:
+		curr, parent = stack.pop()  #get last node added to stack
+
+		#create event dictionary for this comment, add to events list
+		new_event = {'nodeTime': str(int(curr['time'] * 60) + seedTime)}		#convert offset to seconds, add to seed time, convert entire time to string
+		#set nodeid if comment
+		if parent != None:
+			new_event['nodeID'] = "comment" + str(NEXT_ID)
+			NEXT_ID += 1
+		#nodeID = seed post ID if at root
+		else:
+			new_event['nodeID'] = seedID
+		#set parent of this event
+		new_event['parentID'] = seedID if parent == None else parent
+		#set actionType of this event
+		new_event['actionType'] = 'post' if parent == None else 'comment'
+		#add new event to list
+		events.append(new_event)
+
+		visited.add(curr['id'])		#add this node to visited list
+		#append children in reverse time order so final output is sorted (not that it matters)
+		stack.extend([(child, new_event['nodeID']) for child in curr['children']][::-1])   
+
+	return events
+#end tree_to_events
 
 
 #main execution begins here
@@ -103,41 +182,93 @@ for post in raw_post_seeds:
 	post_seeds[post['subreddit']].append(post)
 print({key : len(post_seeds[key]) for key in post_seeds}, "\n")
 
+all_events = []		#list for all sim events, across all seed posts
+
 #process each subreddit
 for subreddit, seeds in post_seeds.items():
+	'''
 	#TESTING ONLY!!!!
-	if subreddit != "Bitcoin":
+	if subreddit != "pivx":
 		continue
+	'''
 
-	print("Processing", subreddit)
+	print("\nProcessing", subreddit, "with", len(seeds), "posts to simulate")
 
 	#load subreddit posts (don't need the comments!)
 	sub_posts = cascade_manip.load_filtered_posts(domain, subreddit)
 	#load subreddit parameters
-	sub_params = cascade_manip.load_cascade_params(domain, subreddit+"100")
+	sub_params = cascade_manip.load_cascade_params(domain, subreddit)
 
-	#filter posts - TESTING ONLY!!!!!!!!
+	#filter posts - TESTING ONLY!!!!!!!! - if you didn't load all the params
+	'''
 	sub_posts = {post_id : post for post_id, post in sub_posts.items() if post_id in sub_params}
 	print("Filtered to", len(sub_posts), "posts with fitted parameters")
+	'''
 
 	#verify loads
 	if sub_posts == False or sub_params == False:
 		print("Load failed - exiting")
 		exit(0)
 
+	#get list of posting users to pull comment user ids from
+	#list, not set, so more frequent posters are more likely to come up
+	user_ids = [post['author_h'] for post_id, post in sub_posts.items() if post['author_h'] != "[Deleted]" ]
+
 	#build graph of these posts/params
-	sub_graph = ParamGraph()
-	sub_graph.build_graph(sub_posts, sub_params)
+	#sub_graph = ParamGraph()
+	#sub_graph.build_graph(sub_posts, sub_params)
+
+	graph_infer = False
 
 	#add all seed posts from this subreddit to graph
-	for post in seeds:
-		sub_graph.add_post(post)
-	print("Updated graph has", sub_graph.graph.number_of_nodes(), "nodes and", sub_graph.graph.size(), "edges")
+	for post in seeds:		
+		#check if we already have params for this post - if not, flag for inference and add post to graph
+		if post['id_h'] not in sub_params:
+			#sub_graph.add_post(post)
+			print("FAIL no params for post")
+			graph_infer = True
+	#print("Updated graph has", sub_graph.graph.number_of_nodes(), "nodes and", sub_graph.graph.size(), "edges")
 
-	#run node2vec to get embeddings
-	sub_graph.run_node2vec()
+	#run node2vec to get embeddings - if we have to infer parameters
+	if graph_infer:
+		sub_graph.run_node2vec()
+
 
 	#for each post, infer parameters and simulate
 	for post in seeds:
-		inferred_params = sub_graph.infer_params(post, 'weighted')
-		print(inferred_params)
+		#if have fitted params, use those
+		if post['id_h'] in sub_params:
+			post_params = sub_params[post['id_h']]
+		#otherwise, infer
+		else:
+			post_params = sub_graph.infer_params(post, 'weighted')
+
+		#simulate a comment tree! just the event times first
+		sim_root, all_times = sim_tree.simulate_comment_tree(post_params)
+
+		#TESTING ONLY!!!! force a result with a couple comments, so I can make sure stuff works
+		'''
+		while len(all_times) < 2:
+			sim_root, all_times = sim_tree.simulate_comment_tree(post_params)
+		'''
+
+		#convert that to desired output format
+		#{"rootID": "t3_-dt8ErhaKuULHekBf_ke3A", "communityID": "t5_2s3qj", "actionType": "post", "parentID": "t3_-dt8ErhaKuULHekBf_ke3A", "nodeTime": "1421194091", "nodeUserID": "XHa80wD0LQJNlMcrUD32pQ", "nodeID": "t3_-dt8ErhaKuULHekBf_ke3A"}
+		'''
+			"rootID": id of root post for cascade
+			"actionType": 'post' or 'comment'
+			"parentID": id of parent (comment or post), null/self if this object is post 
+			"nodeTime": timestamp of event
+			"nodeUserID": id of user posting this event
+			"nodeID": id of post/comment
+			"communityID": id of subreddit			???
+		'''
+		post_events = build_cascade_events(sim_root, post, user_ids)
+
+		#add these events to running list
+		all_events.extend(post_events)
+
+#finished all posts across all subreddit, time to dump
+print("Finished simulation, have", len(all_events), "events to save")
+
+reddit_sim_to_json(domain, all_events, outfile)
