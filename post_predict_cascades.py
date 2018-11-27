a7c967acde2f20a18fe3e3accb146a95af2b2f4c
Rachel Krohn <rkrohn@nd.edu>
2018-11-27 18:10:36 -0500

    added a user_sample mode - sample graph down to a particular size, filtering based on the seed post authors

--- post_predict_cascades.py
+++ post_predict_cascades.py
@@ -14,8 +14,8 @@ import string
 
 
 DISPLAY = False		#toggle debug print statements
-MAX_GRAPH_POSTS = 50	#maximum number of library posts to include in graph for paramter inference
-MAX_TOKEN_MATCH_POSTS = 5		#maximum number of token-matchin posts to add to graph when inferring params for a
+MAX_GRAPH_POSTS = 500	#maximum number of library posts to include in graph for paramter inference if mode != full_graph
+MAX_TOKEN_MATCH_POSTS = 5		#maximum number of token-matching posts to add to graph when inferring params for a
 								#post by an unseen user with all new tokens
 
 #creates submission json file
@@ -185,14 +185,31 @@ def extract_tokens(post):
 print("")
 
 #verify command line args
-if len(sys.argv) != 4:
-	print("Incorrect command line arguments\nUsage: python3 predict_cascades.py <domain> <seed filename> <output filename>")
+if len(sys.argv) < 4:
+	print("Incorrect command line arguments\nUsage: python3 predict_cascades.py <domain> <seed filename> <output filename> <optional mode>")
 	exit(0)
 
 #extract arguments
 domain = sys.argv[1]
 infile = sys.argv[2]
 outfile = sys.argv[3]
+if len(sys.argv) == 5:
+	mode = sys.argv[4]
+else:
+	mode = "full_graph"
+
+#verify valid mode
+if mode != "full_graph" and  mode != "user_sample":
+	print("Invalid graph mode, must be one of: full_graph, user_sample")
+	exit(0)
+
+#print some log-ish stuff in case output being piped and saved
+print("Domain", domain)
+print("Input", infile)
+print("Output", outfile)
+print("Mode", mode)
+print("\nTarget graph size", MAX_GRAPH_POSTS)
+print("Max token match posts", MAX_TOKEN_MATCH_POSTS, "\n")
 
 #load post seeds
 raw_post_seeds = load_reddit_seeds(infile)
@@ -208,11 +225,11 @@ post_counter = 1	#counter of posts to simulate, across all subreddits
 
 #process each subreddit
 for subreddit, seeds in post_seeds.items():
-	
+	'''
 	#TESTING ONLY!!!!
 	if subreddit != "Lisk":
 		continue
-	
+	'''
 
 	print("\nProcessing", subreddit, "with", len(seeds), "posts to simulate")
 
@@ -226,49 +243,129 @@ for subreddit, seeds in post_seeds.items():
 	#no cached, build graph from raw posts and params
 	else:
 		#load subreddit posts (don't need the comments!)
-		sub_posts = cascade_manip.load_filtered_posts(domain, subreddit)
+		raw_sub_posts = cascade_manip.load_filtered_posts(domain, subreddit)
 		#load subreddit parameters
-		sub_params = cascade_manip.load_cascade_params(domain, subreddit)
-
+		raw_sub_params = cascade_manip.load_cascade_params(domain, subreddit)
 
 		#filter posts - TESTING ONLY!!!!!!!! - if you didn't load all the params
 		'''
-		sub_posts = {post_id : post for post_id, post in sub_posts.items() if post_id in sub_params}
+		raw_sub_posts = {post_id : post for post_id, post in sub_posts.items() if post_id in sub_params}
 		print("Filtered to", len(sub_posts), "posts with fitted parameters")
 		'''
 
 		#verify loads
-		if sub_posts == False or sub_params == False:
+		if raw_sub_posts == False or raw_sub_params == False:
 			print("Load failed - exiting")
 			exit(0)
 
 		#remove seed posts from fitted list - no cheating
-		'''
 		for post in seeds:
-			if post['id_h'] in sub_posts:
-				sub_posts.pop(post['id_h'])
-				sub_params.pop(post['id_h'])
-		print(len(sub_posts), "remaining after removing seed posts")
-		'''
+			if post['id_h'] in raw_sub_posts:
+				raw_sub_posts.pop(post['id_h'])
+				raw_sub_params.pop(post['id_h'])
+		print(len(raw_sub_posts), "remaining after removing seed posts")
 
 		#get list of posting users to pull comment user ids from
 		#list, not set, so more frequent posters are more likely to come up
-		user_ids = [post['author_h'] for post_id, post in sub_posts.items() if post['author_h'] != "[Deleted]" ]
+		user_ids = [post['author_h'] for post_id, post in raw_sub_posts.items() if post['author_h'] != "[Deleted]" ]
+
+		#if full graph mode, set sub_posts, and sub_params accordingly
+		if mode == "full_graph":
+			sub_posts = raw_sub_posts
+			raw_sub_params = raw_sub_params
+
+		#if set to user_sample mode, sample down the full post history to only posts by the seed authors
+		#(plus a few more to ensure token connectivity, if at all possible)
+		elif mode == "user_sample":
+			print("Reducing graph size based on seed post authors")
+
+			#set of authors of seed posts
+			authors = set([post['author_h'] for post in seeds])
+			print("  ", len(authors), "authors in seed posts")
+
+			#filter the posts: only those by users in the author pool
+			sub_posts = {key: value for key, value in raw_sub_posts.items() if value['author_h'] in authors}
+			sub_params = {key: value for key, value in raw_sub_params.items() if key in sub_posts}
+
+			graph_authors = set([post['author_h'] for post_id, post in sub_posts.items()])
+			print("   Filtered to", len(sub_posts), "posts by", len(graph_authors), "authors")
+
+			#no more than MAX_GRAPH_POSTS posts, or this will never finish
+			if len(sub_posts) > MAX_GRAPH_POSTS:
+				print("   Sampling down...")
+				#keep at least one post by each author that we have posts for (pick a random one)
+				keep = set()
+				for author in graph_authors:
+					keep.add(random.choice([key for key, value in sub_posts.items() if value['author_h'] == author]))
+				#sample down
+				keep.update(random.sample([key for key in sub_posts.keys() if key not in keep], MAX_GRAPH_POSTS-len(keep)))
+				sub_posts = {key: sub_posts[key] for key in keep}
+				sub_params = {key: sub_params[key] for key in keep}
+			#too few? draw more
+			if len(sub_posts) < MAX_GRAPH_POSTS:
+				print("   Drawing more posts...")
+				#add more
+				draw_keys = [key for key in raw_sub_posts.keys() if key not in sub_posts.keys()]
+				num_draw = MAX_GRAPH_POSTS - len(sub_posts)
+				more = random.sample(draw_keys, num_draw)
+				sub_posts.update({key: raw_sub_posts[key] for key in more})
+				sub_params.update({key: raw_sub_params[key] for key in more})
+
+			graph_authors = set([post['author_h'] for post_id, post in sub_posts.items()])
+			print("   Sampled to", len(sub_posts), "posts by", len(set([post['author_h'] for post_id, post in sub_posts.items()])), "authors for inference (" +  str(len(graph_authors)), "seed authors)")
+
+			#can we connect all the seed posts to this sampled version of the graph? check authors and tokens
+			graph_authors = set([post['author_h'] for post_id, post in sub_posts.items()])	#updated list of graph authors
+
+			#build list of tokens represented in graph (extra work, yes, repeated in graph build)
+			graph_tokens = set()
+			for post_id, post in sub_posts.items():
+				graph_tokens.update(extract_tokens(post))
+
+			#check to see if all seed posts can be connected to this graph
+			for post in seeds:
+				#post author in graph, no need for token match
+				if post['author_h'] in graph_authors:
+					continue
+
+				post_tokens = extract_tokens(post)		#get tokens from this post
+				#if no token connection, try to draw some more posts to allow for connection
+				if len(graph_tokens.intersection(post_tokens)) == 0:
+					#can we find a post in our library with some of these tokens? to get the node connected
+					matching_posts = {key:value for key, value in raw_sub_posts.items() if len(post_tokens.intersection(extract_tokens(value))) != 0}
+					#if too many matching token posts, sample down
+					if len(matching_posts) > MAX_TOKEN_MATCH_POSTS:
+						keep = random.sample(matching_posts.keys(), MAX_TOKEN_MATCH_POSTS)
+						matching_posts = {key: matching_posts[key] for key in keep}
+
+					#if no matching token/user posts - params will basically be a guess
+					if len(matching_posts) == 0:
+						print("   Cannot connect seed post to graph - parameter inference compromised")
+					#add the matching posts to the graph
+					else:
+						print("   Adding", len(matching_posts), "token-matching posts to graph")
+						sub_posts.update(matching_posts)
+
+			graph_authors = set([post['author_h'] for post_id, post in sub_posts.items()])
+			print("   Using", len(sub_posts), "posts by", len(set([post['author_h'] for post_id, post in sub_posts.items()])), "authors for inference (" +  str(len(graph_authors)), "seed authors)")
 
 		#build graph of all posts/params
 		sub_graph = ParamGraph()
 		sub_graph.build_graph(sub_posts, sub_params)
 
 		#pickle this graph, save it for later
+		'''
 		print("Saving post graph to graph_cache/%s_post_graph.pkl and user id list to graph_cache/%s_user_ids.pkl" % (subreddit, subreddit))
 		file_utils.save_pickle(sub_graph, "graph_cache/%s_post_graph.pkl" % subreddit)
 		file_utils.save_pickle(user_ids, "graph_cache/%s_user_ids.pkl" % subreddit)
 		exit(0)
+		'''
 
 	#add all seed posts from this subreddit to graph
+	print("Adding seed posts to graph")
 	for post in seeds:
 		res = sub_graph.add_post(post)
-	print("Updated graph has", sub_graph.graph.number_of_nodes(), "nodes and", sub_graph.graph.size(), "edges")
+	print("   Updated graph has", sub_graph.graph.number_of_nodes(), "nodes and", sub_graph.graph.size(), "edges")
 
 	#run node2vec to get embeddings - if we have to infer parameters
 	sub_graph.run_node2vec()
