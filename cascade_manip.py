dc1017d5631f972a0488f8616686a660a06f6c45
Rachel Krohn <rkrohn@nd.edu>
2018-11-27 14:52:10 -0500

    remove posts with missing comments when filtering comments

--- cascade_manip.py
+++ cascade_manip.py
@@ -62,19 +62,30 @@ def filter_cascades_by_subreddit(cascades, subreddit):
 #given a list of post objects, filter the comments to only those in those cascades
 def filter_comments_by_posts(cascades, comments):
 	comment_ids = set()		#build set of comment ids to include in filtered dictionary
+	posts_to_remove = set()	#set of posts to remove, because they had missing comments
 
 	print("Filtering comments to match posts")
 
 	#loop all posts, built list of comment ids
 	for post_id, post in cascades.items():
-		comment_ids.update(get_cascade_comment_ids(post, comments))		#add this posts's comments to overall list
+		post_comment_ids = get_cascade_comment_ids(post, comments)
+		if post_comment_ids != False:
+			comment_ids.update(post_comment_ids)		#add this posts's comments to overall list
+		else:
+			posts_to_remove.add(post_id)
+
+	#remove some posts if necessary
+	if len(posts_to_remove) != 0:
+		print("Removed", len(posts_to_remove), "posts with missing comments")
+		for post_id in posts_to_remove:
+			cascades.pop(post_id)
 
 	#filter comments to only those in the list
 	filtered_comments = { comment_id : comments[comment_id] for comment_id in comment_ids }
 
 	print("Filtered to", len(filtered_comments), "comments (from", len(comments), "comments)")
 
-	return filtered_comments
+	return cascades, filtered_comments
 #end filter_comments_by_post
 
 
@@ -87,7 +98,11 @@ def get_cascade_comment_ids(post, comments):
 	while len(nodes_to_visit) != 0:
 		curr = nodes_to_visit.pop(0)	#grab current comment id
 		comment_ids.add(curr)			#add this comment to set of cascade comments
-		nodes_to_visit.extend(comments[curr]['replies'])	#add this comment's replies to queue
+		if curr in comments:
+			nodes_to_visit.extend(comments[curr]['replies'])	#add this comment's replies to queue
+		#if no comment with that id, return False and throw out the whole post
+		else:
+			return False
 
 	return comment_ids
 #end get_cascade_comment_ids
