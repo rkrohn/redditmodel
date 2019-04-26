1fd53376b4bf671460dfaaed15001e31adb28b78
Rachel Krohn <rkrohn@nd.edu>
2019-04-26 19:24:34 -0400

    use regular fit procedure to get lists of comment times, so they are shifted properly! (and no more duplicate code)

--- fit_partial_cascade.py
+++ fit_partial_cascade.py
@@ -1,12 +1,13 @@
 from fit_weibull import *
 from fit_lognormal import *
+from fit_cascade_gen_model import *
 
 #given a single cascade and associated comments, and an observed time, fit both the root-comment Weibull and deeper-comment lognormal distributions for the observed comments
 #also, estimate the branching factor
 #does NOT return param quality, because sometimes it doesn't fit at all - and don't need it for sim from partial
 #observed time given in hours
 #param_guess = set of initial params to inform fit process (generally, inferred from post graph)
-def fit_partial_cascade(post, cascade, observed_time, param_guess=False, verbose = False):
+def fit_partial_cascade(post, cascade, observed, observing_time, param_guess=False, verbose = False):
 
     #hackery: declare a special print function for verbose output
     global vprint
@@ -20,18 +21,34 @@ def fit_partial_cascade(post, cascade, observed_time, param_guess=False, verbose
     else:   
         vprint = lambda *a: None      # do-nothing function
 
-    #fit weibull to root comment times
-    root_comment_times = get_root_comment_times(post, cascade)
+    #fitting weibull to root comment times, so get list of those
+    root_comment_times = get_root_comment_times(cascade)
     if root_comment_times == False:
         vprint("Invalid comment times, skipping this cascade.")
         return False
-    #filter to only root comments we've seen
-    root_comment_times = [time for time in root_comment_times if time <= observed_time * 60.0]
     
-    #fit log-normal to all other comment times
-    other_comment_times = get_other_comment_times(post, cascade)
-    #filter to only comments we've seen
-    other_comment_times = [time for time in other_comment_times if time <= observed_time * 60.0]
+    #fitting log-normal to all other comment times, so get list of those
+    other_comment_times = get_other_comment_times(cascade)
+
+    #filter comment lists to only those we've observed for fit process
+    #if observing based on time, filter each list separately
+    if observing_time:
+        root_comment_times = [time for time in root_comment_times if time <= observed * 60.0]
+        other_comment_times = [time for time in other_comment_times if time <= observed * 60.0]
+    #if observing only the first N comments, need to consider root and other comments together
+    else:
+        #get list of ALL comments and root/other identifier
+        all_comment_times = [(time, "root") for time in root_comment_times]
+        all_comment_times += [(time, "other") for time in other_comment_times]
+
+        #sort this list by time
+        all_comment_times = sorted(all_comment_times, key=lambda x: x[0])
+        #pull just the observed comments
+        observed_comments = all_comment_times[:observed]
+
+        #filter root/other lists to contain only those first n comments
+        root_comment_times = root_comment_times[:sum(1 for comment in observed_comments if comment[1] == "root")]
+        other_comment_times = other_comment_times[:sum(1 for comment in observed_comments if comment[1] == "other")]
 
     #how many comments have we observed?
     observed_count = len(root_comment_times) + len(other_comment_times)
@@ -61,58 +78,6 @@ def fit_partial_cascade(post, cascade, observed_time, param_guess=False, verbose
     #return all parameters together - your job to keep the order straight ;) - [a, lbd, k, mu, sigma, n_b]
     return params
 #end fit_partial_cascade
-
-
-#given a post and its reconstructed cascade, return sorted list of post (root) reply times in minutes (originally stored in seconds),
-#taking the post time as 0 and offsetting the comment times accordingly
-def get_root_comment_times(post, cascade):
-    root_comment_times = []     #build list of reply times
-
-    root_time = post['time']     #get post time in seconds to use as offset
-
-    #loop replies
-    for comment in cascade['replies']:
-        #if comment occurs less than 60 seconds before the post (assume clock weirdness), set to 0 and move to next
-        if comment['time'] - root_time < 0 and root_time - comment['time'] < 60:
-            root_comment_times.append(0.0)
-        #if comment occurs more than a minute before the post, error and quit
-        elif comment['time'] - root_time < 0:
-            print("NEGATIVE COMMENT TIME - FAIL!!!!")
-            print(post)
-            print("root time", root_time)
-            reply_times = []
-            for ident in post['replies']:
-                reply_times.append(comments[ident]['created_utc'])
-            print("reply times", sorted(reply_times))
-            return False
-            
-        root_comment_times.append((comment['time'] - root_time) / 60)       #get time between post and comment in minutes
-
-    root_comment_times.sort()   #sort by time
-    return root_comment_times
-#end get_root_comment_times
-
-
-#get list of comment times of all comments not on post (root) in minutes, offset based on the post time
-def get_other_comment_times(post, cascade):
-    other_comment_times = []    #list of comment times
-
-    root_time = post['time']     #get post time in seconds to use as offset
-
-    #get list of comment ids that are on the root, so we can exclude them
-    root_comment_ids = [comment['id'] for comment in cascade['replies']]      #replies to root
-    #build list of all non-root replies for this post
-    other_comment_ids = []
-    comments_to_visit = [] + cascade['replies']   #init queue to direct post replies
-    while len(comments_to_visit) != 0:
-        curr = comments_to_visit.pop(0)    #grab current comment
-        if curr['id'] not in root_comment_ids:
-            other_comment_times.append((curr['time'] - root_time) / 60)     #add comment time to list
-        comments_to_visit.extend(curr['replies'])    #add this comment's replies to queue
-
-    other_comment_times.sort()  #sort by time
-    return other_comment_times
-#end get_other_comment_times
 
 
 #estimate branching number n_b for partial cascade: 1 - (root degree / total comments)
