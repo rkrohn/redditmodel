ceb34af4be264589dd7b1d5092859e73c24ca36b
Rachel Krohn <rkrohn@nd.edu>
2019-05-01 14:09:10 -0400

    some minor corrections to crypto/cve data loading, handle some rogue comments

--- socsim_data_functions_gen_cascade_model.py
+++ socsim_data_functions_gen_cascade_model.py
@@ -61,7 +61,7 @@ def load_data(domain):
 
 	#testing data
 	test_posts, test_cascades, test_params, test_failed_fit_posts = filter_data_by_ids(test_ids, posts, cascades, params, failed_fit_posts)
-	vprint("Filtered training data to %d posts, %d cascades, and %d params" % (len(test_posts), len(test_cascades), len(test_params)))
+	vprint("Filtered testing data to %d posts, %d cascades, and %d params" % (len(test_posts), len(test_cascades), len(test_params)))
 	vprint("   %d failed fit posts" % len(test_failed_fit_posts))
 
 	return train_posts, train_cascades, train_params, train_failed_fit_posts, test_posts, test_cascades, test_params, test_failed_fit_posts
@@ -86,17 +86,20 @@ def load_all_data(domain):
 
 	#cascades
 	cascades = get_cascades(domain, posts)
+	vprint("   Loaded %d cascades" % len(cascades))
 
 	#throw out posts that we don't have cascades for - incomplete or some other issue
 	if len(posts) != len(cascades):
 		posts, deleted_count = functions_gen_cascade_model.filter_dict_by_list(posts, list(cascades.keys()), num_deleted=True)
 		vprint("   Deleted %d posts without cascades" % deleted_count)
-
+		cascades, deleted_count = functions_gen_cascade_model.filter_dict_by_list(cascades, list(posts.keys()), num_deleted=True)
+		vprint("   Deleted %d cascades without posts" % deleted_count)
+	
 	#params
 	#load if params file exists
 	if file_utils.verify_file(fitted_params_filepath % (domain, domain)):
 		params_data = file_utils.load_pickle(fitted_params_filepath % (domain, domain))
-		vprint("   Loaded %d fitted params" % len(params_data['params_dict']))
+		vprint("   Loaded %d fitted params (%d failed fit)" % (len(params_data['params_dict']), len(params_data['failed_fit_list'])))
 	#if params file doesn't exist, create it - loading in the process
 	else:
 		vprint("   Fitted params file doesn't exist, creating now")
@@ -109,6 +112,8 @@ def load_all_data(domain):
 	if len(posts) != len(params)+len(failed_fit_posts):
 		posts, deleted_count = functions_gen_cascade_model.filter_dict_by_list(posts, list(params.keys())+failed_fit_posts, num_deleted=True)
 		vprint("   Deleted %d posts without params" % deleted_count)
+		params, deleted_count = functions_gen_cascade_model.filter_dict_by_list(params, list(posts.keys()), num_deleted=True)
+		vprint("   Deleted %d params without posts" % deleted_count)
 
 	vprint("Loaded %d posts " % len(posts))
 	vprint("   %d cascades" % len(cascades))
@@ -133,9 +138,8 @@ def process_posts(domain):
 	posts = {}
 	for post_id, post_object in raw_posts.items():
 		#check for good post, fail and error if something is amiss
-		if post_object['title_m'] is None or post_object['created_utc'] is None or post_object['author_h'] is None or post_object['created_utc'] is None:
+		if post_object['title_m'] is None or post_object['created_utc'] is None or post_object['author_h'] is None:
 			print("Skipping invalid post %s" % post_id)
-			continue
 
 		#build new post dict
 		post = {}
@@ -148,13 +152,14 @@ def process_posts(domain):
 		post['subreddit'] = post_object['subreddit']	#extra field for just this data - in case we need/want it later
 
 		#add to overall post dict
-		real_post_id = post_object['name_h']		#post id with t3_ prefix
+		if post_object['name_h'] is None:
+			real_post_id = "t3_" + post_id
+		else:
+			real_post_id = post_object['name_h']		#post id with t3_ prefix
 		posts[real_post_id] = post
 
 	#save to pickle 
 	file_utils.save_pickle(posts, processed_posts_filepath % (domain, domain))
-
-	vprint("Processed %d posts" % len(posts))
 
 	return posts
 #end process_posts
@@ -194,9 +199,34 @@ def load_comments(domain, posts):
 
 	#convert to our nested dictionary structure, filtering out irrelevant comments along the way
 	for comment_id, comment_object in raw_comments.items():
+		#if comment object link and parent fields don't have the t3_/t1_ prefix, add it now
+		#post parent always a post with t3_ prefix
+		if comment_object['link_id_h'][:3] != "t3_":
+			comment_object['link_id_h'] = "t3_" + comment_object['link_id_h']
+		#immediate parent could be a post or a comment
+		if comment_object['parent_id_h'][:3] != "t3_" and comment_object['parent_id_h'][:3] != "t1_":
+			#if link and parent match, immediate parent is a post
+			if "t3_"+comment_object['parent_id_h'] == comment_object['link_id_h']:
+				comment_object['parent_id_h'] = "t3_" + comment_object['parent_id_h']
+			#otherwise, immediate parent is comment
+			else:
+				comment_object['parent_id_h'] = "t1_" + comment_object['parent_id_h']
 
 		#skip comment if not for post in set
 		if comment_object['link_id_h'] not in post_ids:
+			print("   No post parent for this comment, skipping")
+			continue
+
+		#special-case for these two rogue crypto comments - hardcoding a time as the midpoint of the
+		#parent and the child
+		if comment_id == 'YlNE1IsFAufmn-RJJE25mw':
+			comment_object['created_utc'] = 1431117009
+		if comment_id == 'P3hMh-ZJJjG52smrScH76g':
+			comment_object['created_utc'] = 1448843362
+
+		#if time field is empty, skip this comment
+		if comment_object['created_utc'] is None:
+			print("   No time for this comment, skipping")
 			continue
 
 		#build new comment dict
@@ -279,9 +309,9 @@ def load_set_list(domain):
 
 #given posts, cascades, params, and failed fit list, filter to match a set of post_ids
 def filter_data_by_ids(id_set, posts, cascades, params, failed_fit_posts):
-	posts = functions_gen_cascade_model.filter_dict_by_list(deepcopy(posts), id_set)
-	cascades = functions_gen_cascade_model.filter_dict_by_list(deepcopy(cascades), id_set)
-	params = functions_gen_cascade_model.filter_dict_by_list(deepcopy(params), id_set)
+	posts = functions_gen_cascade_model.filter_dict_by_list(posts, id_set)
+	cascades = functions_gen_cascade_model.filter_dict_by_list(cascades, id_set)
+	params = functions_gen_cascade_model.filter_dict_by_list(params, id_set)
 	failed_fit_posts = [post_id for post_id in failed_fit_posts if post_id in id_set]
 
 	return posts, cascades, params, failed_fit_posts
