72f6f266107efb8b5a21aa90b8420cbd42f46ae2
Rachel Krohn <rkrohn@nd.edu>
2019-06-06 11:45:03 -0400

    update to use new sampling scheme, pass file identifier as argument instead of using timestamps, reorder output file fields

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,3 @@
-import datetime
 import subprocess
 from collections import defaultdict
 import sys
@@ -14,12 +13,14 @@ arguments_list = []
 
 repeat_runs = 1			#number of repeated runs to do for each subreddit/size class
 
+#first command line arg - string identifier for this set of runs
+run_str = sys.argv[1]
 #list of subreddits separately, since one output file per subreddit
 #subreddits = ['explainlikeimfive']	#list of subreddits, -s
 #converted this to a command line arg for parallel processing of multiple subreddits,
 #even during the preprocessing phase
 #but it could be manual without too much work
-subreddits = sys.argv[1:]
+subreddits = sys.argv[2:]
 
 #list of times/comment counts for observation, along with the selected option
 observation_option = '-nco'		#-nco or -t
@@ -30,8 +31,11 @@ observation_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
 arguments_list.append('-j')		#-j, -c, or -wmd
 
 #define the test set: -n <sample size>, -id <post id>, -r, or -a
-arguments_list.append('-a')		#list for -a or -r
-#arguments['-n'] = 50			#dict for -n or -id
+#arguments_list.append('-a')		#list for -a or -r
+arguments['-n'] = 1000			#dict for -n or -id
+
+#define the size of the training set
+arguments['-n_train'] = 10000
 
 #choose at least one graph limit option, but can use both
 arguments['-topn'] = 20			#max connected neighbors (pick highest weighted edges)
@@ -47,7 +51,8 @@ arguments_list.append('--topo_err')
 #size classes - put a list of size category breaks here, and the loop will process them all
 #first will just be used as a max, last as just a min
 #for options -min and -max
-size_breaks = [1, 5, 10, 30, 50, 100, 200, 300, 500, 1000]
+#size_breaks = [1, 5, 10, 30, 50, 100, 200, 300, 500, 1000]
+size_breaks = []
 
 #OPTIONAL ARGUMENTS: only add to dictionary/list if not using default
 
@@ -58,9 +63,9 @@ size_breaks = [1, 5, 10, 30, 50, 100, 200, 300, 500, 1000]
 arguments['-np'] = 'ln'		#optional normalization, ln (natural log) or mm (min-max)
 
 #must do both of these together, if you want any
-#arguments['-down_ratio'] = 3	#ratio of large:small posts for graph downsampling, 
+arguments['-down_ratio'] = 1	#ratio of large:small posts for graph downsampling, 
 								#where large/small border defined by -large_req
-#arguments['-large_req'] = 20	#divider between large and small cascades for downsample
+arguments['-large_req'] = 10	#divider between large and small cascades for downsample
 
 #arguments_list.append('-e')		#estimate initial params
 arguments_list.append('-v')			#verbose output
@@ -78,10 +83,6 @@ arguments_list.append('-d')		#include default posts in graph
 for subreddit in subreddits:
 	file_utils.verify_dir("sim_results/%s" % subreddit)
 
-#timestamp for this set of runs
-timestamp = '{:%m-%d-%H:%M:%S}'.format(datetime.datetime.now())
-#timestamp = "05-15-21:46:48"		#hardcode to fill holes caused by process death
-
 #prepend/append a None to size breaks list for easier looping
 size_breaks = [None] + size_breaks + [None]
 
@@ -96,11 +97,13 @@ for run in range(repeat_runs):
 
 		#string representation of this size class
 		if min_size is not None and max_size is not None:
-			size_class = "%d-%d" % (min_size, max_size)
+			size_class = "_%d-%d" % (min_size, max_size)
 		elif min_size is not None:
-			size_class = ">=%d" % min_size
+			size_class = "_>=%d" % min_size
+		elif max_size is not None:
+			size_class = "_<%d" % max_size
 		else:
-			size_class = "<%d" % max_size
+			size_class = ""
 
 		#and set the min and max in the arguments dict accordingly
 		#remove old settings
@@ -120,7 +123,7 @@ for run in range(repeat_runs):
 		for subreddit in subreddits:
 
 			#define our base output filename - keep it simple, will have all the settings in the output files
-			outfile = "sim_results/%s/%s_%d-%d_%s_test_%s%s" % (subreddit, subreddit, arguments['-y'], arguments['-m'], size_class, timestamp, "_run%d" % run if repeat_runs > 1 else "")
+			outfile = "sim_results/%s/%s_%s_%d-%d%s_test%s" % (subreddit, subreddit, run_str, arguments['-y'], arguments['-m'], size_class, "_run%d" % run if repeat_runs > 1 else "")
 
 			#this run already done? skip
 			#check the bookmark saved by the model to know if finished or not
@@ -147,7 +150,7 @@ for run in range(repeat_runs):
 			#wait for this graph-build-only run to finish before doing more
 			if sub_counts[subreddit] == 0:
 				print("Preprocessing", subreddit)
-				f = open("sim_results/%s/%s_%d-%d_%sgraph.txt" % (subreddit, subreddit, arguments['-y'], arguments['-m'], timestamp), "a")
+				f = open("sim_results/%s/%s_%s_%d-%dgraph.txt" % (subreddit, subreddit, run_str, arguments['-y'], arguments['-m']), "a")
 				subprocess.call(command+['-preprocess'], stdout=f, stderr=f)
 				print("Done")
 
